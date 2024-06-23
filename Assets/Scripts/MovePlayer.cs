2473097bb516864763648df035214b1232eb5e6c
archost <harisov.h.h@mail.ru>
2024-06-24 00:17:11 +0500

    Vosk setup
    - added speech recognition via vosk
    - added simple test with moving cube

---
+++ Assets/Scripts/MovePlayer.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlayer : MonoBehaviour
+{
+    public void moveRight() => transform.Translate(Vector3.right);
+
+    public void moveLeft() => transform.Translate(Vector3.left);
+
+    public void moveUp() => transform.Translate(Vector3.up);
+
+    public void moveDown() => transform.Translate(Vector3.down);
+}
---
+++ Assets/Scripts/Vosk/VoskDialogText.cs
@@ -0,0 +1,63 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Text.RegularExpressions;
+
+public class VoskDialogText : MonoBehaviour 
+{
+	[SerializeField]
+	private MovePlayer player;
+
+    public VoskSpeechToText VoskSpeechToText;
+    public Text DialogText;
+
+	Regex left_regex = new Regex(@"влево|лево");
+	Regex right_regex = new Regex(@"вправо|право");
+	Regex up_regex = new Regex(@"вверх|верх");
+	Regex down_regex = new Regex(@"вниз|низ");
+
+    void Awake()
+    {
+        VoskSpeechToText.OnTranscriptionResult += OnTranscriptionResult;
+    }
+
+	void AddResponse(string response) {
+		DialogText.text = response + "\n\n";
+	}
+
+    private void OnTranscriptionResult(string obj)
+    {
+        Debug.Log("Сказал: " + obj);
+        var result = new RecognitionResult(obj);
+
+        foreach (RecognizedPhrase p in result.Phrases)
+        {
+			if (left_regex.IsMatch(p.Text))
+			{
+				player.moveLeft();
+				AddResponse("Движение влево");
+				return;
+			}
+            if (right_regex.IsMatch(p.Text))
+            {
+                player.moveRight();
+                AddResponse("Движение вправо");
+                return;
+            }
+            if (up_regex.IsMatch(p.Text))
+            {
+                player.moveUp();
+                AddResponse("Движение вверх");
+                return;
+            }
+            if (down_regex.IsMatch(p.Text))
+            {
+                player.moveDown();
+                AddResponse("Движение вниз");
+                return;
+            }
+        }
+		if (result.Phrases.Length > 0 && result.Phrases[0].Text != "") {
+			AddResponse("я тебя не понимаю");
+		}
+    }
+}
